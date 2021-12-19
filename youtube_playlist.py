fa3a84279f756dd334c6a8020b2982ce70ff6210
Chris Idema <48433711+ChrisIdema@users.noreply.github.com>
2021-12-19 19:04:49 +0100

    added minutes, prints as list of tuples

--- youtube_playlist.py
+++ youtube_playlist.py
@@ -6,66 +6,15 @@ html_filename = 'food - YouTube.htm'
 regex_uid = r"(?<=v=)[^&]*"
 regex_index = r"(?<=&index=)\d*"
 
-'''
-#prints index, video uid and video title
-with open(html_filename, encoding="utf8") as fp:
-    soup = BeautifulSoup(fp, "html.parser")
-    for link in soup.find_all('a'):
-        link_id = link.get('id')
-        #print(link)
-        if link_id == "video-title":    
-            title = link.get('title')
-            href = link.get('href')
-            
-            #print(link_id)
-            #print(title)
-            #print(href)
-            
-            uid_find = re.search(regex_uid, href)
-            #if uid_find != None:
-            uid = uid_find.group()
-            #print(uid)  
-            
-            index_find = re.search(regex_index, href)
-            #if index_find != None:
-            index = index_find.group()
-            #print(index)  
-            
-            print(index, uid, title)
-            #print(link)
-                
-            
-        #    print(link)
-        #print(link.get('href'))
-'''
-
-'''
-#prints own channel name too
-with open(html_filename, encoding="utf8") as fp:
-    soup = BeautifulSoup(fp, "html.parser")
-    for a in soup.find_all('a'):
-        #print(a)
-        classes = a.get('class')
-        #print(classes)
-        #print(a.text)
-        #"yt-simple-endpoint style-scope yt-formatted-string"
-        if classes != None and "yt-formatted-string" in classes:
-            #print(div)   
-            #print(div.get('id'))
-            print(a.text)
-            # print(a)
-'''
-
-
-#uses grand parent tag of video url to find channel name of video            
+video_list = []
+     
 with open(html_filename, encoding="utf8") as fp:
     soup = BeautifulSoup(fp, "html.parser")
     for link in soup.find_all('a'):
         link_id = link.get('id')
 
-        classes = link.get('class')
-
-        if link_id == "video-title":    
+        if link_id == "video-title":
+            #get title:    
             title = link.get('title')
 
             href = link.get('href')
@@ -78,13 +27,12 @@ with open(html_filename, encoding="utf8") as fp:
             index_find = re.search(regex_index, href)
             index = index_find.group()
 
+            #get duration
             time_span_id = 'ytd-thumbnail-overlay-time-status-renderer'
-            for parent_span in link.parent.parent.parent.find_all('span'):
-                classes = parent_span.get('class')
-                if classes != None and time_span_id in classes:
-                    print(parent_span.text.strip())
+            ggp_spans = link.parent.parent.parent.find_all('span')
+            duration = next(filter(lambda s: s.get('class') != None and time_span_id in s.get('class'), ggp_spans), None).text.strip()
 
-
+            #print(duration)
 
             #extract channel name from grand parent
             for grandparent_link in link.parent.parent.find_all('a'):
@@ -93,3 +41,19 @@ with open(html_filename, encoding="utf8") as fp:
                     channel_name = grandparent_link.text                    
                     #print(index, uid, title, channel_name)
 
+            channel_name_class = 'yt-formatted-string'
+            gp_links = link.parent.parent.find_all('a')
+            channel_name = next(filter(lambda l: l.get('class') != None and channel_name_class in l.get('class'), gp_links), None).text
+            #print(channel_name)
+
+            #extract channel name from grand parent
+            for grandparent_link in link.parent.parent.find_all('a'):
+                classes = grandparent_link.get('class')
+                if classes != None and "yt-formatted-string" in classes:
+                    channel_name = grandparent_link.text                    
+                    #print(index, uid, title, channel_name)
+
+            #print(index, uid, title, duration, channel_name)
+            video_list.append((index, uid, title, duration, channel_name))
+
+print(video_list)
