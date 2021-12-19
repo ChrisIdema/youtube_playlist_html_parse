cadff6d118c614c8c9142966c0ae4e36d80e2f63
Chris Idema <48433711+ChrisIdema@users.noreply.github.com>
2021-12-19 18:24:46 +0100

    prints channel name too

--- youtube_playlist.py
+++ youtube_playlist.py
@@ -6,11 +6,13 @@ html_filename = 'food - YouTube.htm'
 regex_uid = r"(?<=v=)[^&]*"
 regex_index = r"(?<=&index=)\d*"
 
+'''
+#prints index, video uid and video title
 with open(html_filename, encoding="utf8") as fp:
     soup = BeautifulSoup(fp, "html.parser")
     for link in soup.find_all('a'):
         link_id = link.get('id')
-        #print(link_id)
+        #print(link)
         if link_id == "video-title":    
             title = link.get('title')
             href = link.get('href')
@@ -30,11 +32,56 @@ with open(html_filename, encoding="utf8") as fp:
             #print(index)  
             
             print(index, uid, title)
+            #print(link)
                 
             
         #    print(link)
         #print(link.get('href'))
-        
+'''
+
+'''
+#prints own channel name too
+with open(html_filename, encoding="utf8") as fp:
+    soup = BeautifulSoup(fp, "html.parser")
+    for a in soup.find_all('a'):
+        #print(a)
+        classes = a.get('class')
+        #print(classes)
+        #print(a.text)
+        #"yt-simple-endpoint style-scope yt-formatted-string"
+        if classes != None and "yt-formatted-string" in classes:
+            #print(div)   
+            #print(div.get('id'))
+            print(a.text)
+            # print(a)
+'''
 
 
+#uses grand parent tag of video url to find channel name of video            
+with open(html_filename, encoding="utf8") as fp:
+    soup = BeautifulSoup(fp, "html.parser")
+    for link in soup.find_all('a'):
+        link_id = link.get('id')
+
+        classes = link.get('class')
+
+        if link_id == "video-title":    
+            title = link.get('title')
+
+            href = link.get('href')
+            
+            #extract video uid from href:
+            uid_find = re.search(regex_uid, href)
+            uid = uid_find.group()
+            
+            #extract video playlist index from href
+            index_find = re.search(regex_index, href)
+            index = index_find.group()
+
+            #extract channel name from grand parent
+            for grandparent_link in link.parent.parent.find_all('a'):
+                classes = grandparent_link.get('class')
+                if classes != None and "yt-formatted-string" in classes:
+                    channel_name = grandparent_link.text
+                    print(index, uid, title, channel_name)
 
